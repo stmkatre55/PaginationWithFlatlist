bb3fb97cc02b1eac0ce32fd2f2c2706dca61ab67
Satyam Katre <stmkatre55@.com>
2023-04-28 01:56:17 +0530

    Commit

--- src/screens/Details.js
+++ src/screens/Details.js
@@ -1,28 +1,170 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import React, { useState } from 'react';
 import {
-    Text, View, Image, Dimensions
+    LayoutAnimation, Text, View, Image, Dimensions, StatusBar, ImageBackground, ScrollView, TouchableOpacity
 } from 'react-native';
-import BackIcon from 'react-native-vector-icons/MaterialIcons';
+import BackIcon from 'react-native-vector-icons/Feather';
+import MenuIcon from 'react-native-vector-icons/Entypo';
+import ExpandIcon from 'react-native-vector-icons/Feather';
 
-const {width, height} = Dimensions.get('screen');
+const { width, height } = Dimensions.get('screen');
 
 const Details = (props) => {
-    const {item} = props.route.params;
+    const { item } = props.route.params;
     const navigation = useNavigation();
+    const [expanded, setExpanded] = useState(true);
 
 
-    return(
-        <View style={{flex:1, backgroundColor:'#fff'}}>
-            <View style={{padding:'3%', elevation:5, backgroundColor:'#fff', marginBottom:'3%', flexDirection:'row', alignItems:'center'}}>
-                <BackIcon name='keyboard-arrow-left' size={30} color={'#000'} onPress={() => navigation.goBack()} />
-                <Text style={{ fontSize: 20, color: '#333', fontWeight: '800', marginLeft:'2%' }}>Details</Text>
-            </View>
-            <Image source={{uri: item.url}} style={{width:width/1.05, alignSelf:'center', borderRadius:10, elevation:5, height:height/4.5}} />
-            <View style={{width:width/1.05, alignSelf:'center', justifyContent: 'flex-start', alignItems: 'flex-start', marginVertical:'2%' }}>
-                    <Text style={{ fontSize: 15, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+    const changeLayout = () => {
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+        setExpanded(!expanded);
+    };
+
+
+    return (
+        <View style={{ flex: 1,}}>
+            <StatusBar translucent backgroundColor={'transparent'} />
+            <ImageBackground source={{ uri: item.url }}
+                style={{ height: height, paddingBottom: 0, paddingTop: height / 18, justifyContent: 'space-between' }}>
+                <View style={{ paddingLeft: '5%', marginBottom: '3%', flexDirection: 'row', justifyContent: 'space-between' }}>
+                    <BackIcon name='arrow-left' size={30} color={'#000'} onPress={() => navigation.goBack()} />
+                </View>
+                <View style={{
+                    alignItems: 'flex-end',
+                    // paddingHorizontal: 5,
+                    width: width,
+                    alignSelf: 'center',
+                    justifyContent: 'flex-end',
+                    alignSelf: 'flex-end',
+                    paddingTop: Platform.OS === 'ios' ? 20 : 0,
+                }}>
+                    <View style={{
+                        borderTopLeftRadius: 30,
+                        borderTopRightRadius: 30,
+                        justifyContent: 'flex-end',
+                        backgroundColor: '#fff'
+                    }}>
+                        <View
+                         style={{
+                            paddingBottom: height / 40,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}>
+                        {expanded ? (
+                            <View
+                                style={{
+                                    justifyContent: 'center',
+                                    alignContent: 'center',
+                                    alignItems: 'center',
+                                }}>
+                                <TouchableOpacity
+                                    activeOpacity={0.9}
+                                    onPress={() => changeLayout()}
+                                    style={{
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                    }}
+                                >
+                                    <View style={{ height: 8, backgroundColor: 'red', borderColor: '#fc9fc4', width: 40, borderRadius: 10, marginVertical: '3%' }} />
+                                </TouchableOpacity>
+                                <View style={{ flexDirection: 'row', width: width / 1.2 }}>
+                                    <View style={{ width: '80%', justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+
+                                        <Text style={{ fontSize: 24, color: '#333', fontWeight: '800' }}>{item.title}</Text>
+                                    </View>
+                                    <View style={{ width: '20%', justifyContent: 'center', alignItems: 'center' }}>
+                                        <MenuIcon name='heart-outlined' size={30} color={'#000'} />
+                                    </View>
+                                </View>
+                            </View>
+                        ) : (
+                            <View
+                                style={{
+                                    justifyContent: 'center',
+                                    alignContent: 'center',
+                                    alignItems: 'center',
+                                }}>
+                                <TouchableOpacity
+                                    activeOpacity={0.9}
+                                    onPress={() => changeLayout()}
+                                    style={{
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                    }}
+                                >
+                                    <View style={{ height: 8, backgroundColor: 'red', borderColor: '#fc9fc4', width: 40, borderRadius: 10, marginVertical: '3%' }} />
+                                </TouchableOpacity>
+                                <View style={{ flexDirection: 'row', width: width / 1.2 }}>
+                                    <View style={{ width: '80%', justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+
+                                        <Text style={{ fontSize: 24, color: '#333', fontWeight: '800' }}>{item.title}</Text>
+                                    </View>
+                                    <View style={{ width: '20%', justifyContent: 'center', alignItems: 'center' }}>
+                                        <MenuIcon name='heart-outlined' size={30} color={'#000'} />
+                                    </View>
+                                </View>
+                            </View>
+                        )}
+                    </View>
+
+                    <View
+                        style={{
+                            height: expanded ? null : 0,
+                            backgroundColor: "#fff",
+                            overflow: 'hidden',
+                            width: width,
+                            minWidth: width,
+                            maxHeight: height / 1.8,
+                            minHeight: height / 15
+                        }}>
+                        <ScrollView showsVerticalScrollIndicator={false}>
+                            <View
+                                style={{
+                                    width:width/1.1,
+                                    alignSelf:'center',
+                                    paddingBottom: height / 20,
+                                }}>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text><Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                                <Text style={{ fontSize: 16, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+
+                            </View>
+                        </ScrollView>
+                    </View>
                 </View>
         </View>
+            </ImageBackground >
+    {/* <Image source={{uri: item.url}} style={{width:width/1.05, alignSelf:'center', borderRadius:10, elevation:5, height:height/4.5}} /> */ }
+{/* <View style={{ width: width / 1.05, alignSelf: 'center', justifyContent: 'flex-start', alignItems: 'flex-start', marginVertical: '2%' }}>
+                <Text style={{ fontSize: 15, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+            </View> */}
+        </View >
     );
 }
 export default Details;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,12 +4,15 @@ import {
     Dimensions,
     FlatList,
     Image,
+    StatusBar,
     Text, TouchableOpacity, View
 } from 'react-native';
 import axios from 'axios';
 import { useNavigation } from '@react-navigation/native';
+import BackIcon from 'react-native-vector-icons/Feather';
+import MenuIcon from 'react-native-vector-icons/Entypo';
 
-
+const { width, height } = Dimensions.get('screen');
 const Home = () => {
     const [data, setData] = useState();
     const [loading, setLoading] = useState(false);
@@ -31,24 +34,28 @@ const Home = () => {
 
     const renderItem = ({ item }) => {
         return (
-            <TouchableOpacity onPress={() => navigation.navigate("Details",{item : item})}
-            style={{ padding: '2%', width: Dimensions.get('screen').width / 1.05, borderRadius: 10, borderWidth: 1, alignSelf: 'center', marginVertical: '1%', flexDirection: 'row' }}>
-                <View style={{ width: '25%', justifyContent: 'flex-start', alignItems: 'flex-start' }}>
-                    <Image source={{ uri: item.thumbnailUrl }} style={{ width: 60, height: 60, borderRadius: 10 }} />
+            <TouchableOpacity onPress={() => navigation.navigate("Details", { item: item })}
+                style={{ padding: '2%', width: Dimensions.get('screen').width / 1.1, borderRadius: 10, alignSelf: 'center', marginVertical: '2%', flexDirection: 'row' }}>
+                <View style={{ width: '22%', justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+                    <Image source={{ uri: item.thumbnailUrl }} style={{ width: 60, height: 60, borderRadius: 15 }} />
                 </View>
-                <View style={{ width: '75%', justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+                <View style={{ width: '60%', justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+                    <View style={{borderTopWidth:3, borderColor:'#fc9fc4', width:20}} />
                     <Text style={{ fontSize: 15, color: '#333', fontWeight: '400' }}>{item.title}</Text>
+                </View>
+                <View style={{ width: '18%', justifyContent: 'center', alignItems: 'center' }}>
+                    <MenuIcon name='heart-outlined' size={30} color={'#000'} />
                 </View>
             </TouchableOpacity>
         )
     }
 
     const renderFooter = () => {
-        if (enddata.length > 0){
+        if (enddata.length > 0) {
             return (
                 <ActivityIndicator color={'red'} size={'large'} />
             )
-        }else {
+        } else {
             return null;
         }
     }
@@ -75,9 +82,22 @@ const Home = () => {
     }
 
     return (
-        <View style={{ flex: 1, backgroundColor:'#fff',}}>
-            <View style={{padding:'3%', elevation:5, backgroundColor:'#fff', marginBottom:'3%', flexDirection:'row', alignItems:'center', justifyContent:'center'}}>
-                <Text style={{ fontSize: 20, color: '#333', fontWeight: '800', marginLeft:'2%' }}>Welcome!</Text>
+        <View style={{ flex: 1, backgroundColor: '#fce8f0', }}>
+            <StatusBar backgroundColor={'#fce8f0'} barStyle={'dark-content'} />
+            <View style={{ padding: '3%', paddingHorizontal: '5%', marginBottom: '3%', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                <BackIcon name='arrow-left' size={30} color={'#000'} onPress={() => navigation.goBack()} />
+                <MenuIcon name='dots-three-horizontal' size={30} color={'#000'} />
+            </View>
+            <View style={{ flexDirection: 'row', width: width / 1.15, alignSelf: 'center', justifyContent: 'space-between', marginVertical: '2%' }}>
+                <View style={{ width: '60%', justifyContent: 'center' }}>
+                    <Text style={{ fontSize: 18, color: '#333', fontWeight: '400', opacity: 0.5 }}>Today</Text>
+                    <Text style={{ fontSize: 14, color: '#333', fontWeight: '400' }}>11 February</Text>
+                </View>
+                <View style={{ borderLeftWidth: 1, marginHorizontal: '5%', marginVertical: '2%' }} />
+                <View style={{ width: '35%', justifyContent: 'center' }}>
+                    <Text style={{ fontSize: 10, color: '#333', fontWeight: '400', opacity: 0.5 }}>Select activity</Text>
+                    <Text style={{ fontSize: 12, color: '#333', fontWeight: '400' }}>gym</Text>
+                </View>
             </View>
             <FlatList
                 data={data}
@@ -86,6 +106,7 @@ const Home = () => {
                 ListFooterComponent={() => renderFooter()}
                 onEndReachedThreshold={0.1}
                 onEndReached={() => handleLoadMore()}
+                contentContainerStyle={{ width: width, marginTop: '5%', borderTopLeftRadius: 30, borderTopRightRadius: 30, backgroundColor: '#fff', padding: '3%', elevation: 5 }}
             />
         </View>
     );
